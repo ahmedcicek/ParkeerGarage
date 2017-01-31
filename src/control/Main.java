21489e9555b178ac5802b688e0b354db40065fa8
Ahmed Çiçek <ahmed.46@live.nl>
2017-01-31 10:42:37 +0100

    hoi jongens

---
+++ src/control/Main.java
@@ -0,0 +1,11 @@
+package control;
+
+/**
+ * Created by Ahmed Çiçek on 27-1-2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+        simulator.run();
+    }
+}
